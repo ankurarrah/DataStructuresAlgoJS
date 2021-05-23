5e732bee7416b0cd1407ef5068d3ccc6d12b4b6e
Ankur Singh <ankusin@microsoft.com>
2021-05-23 15:02:05 +0530

    3:01 PM 23 05 21

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,32 +1,30 @@
-//https://www.geeksforgeeks.org/selection-sort/
+//https://www.geeksforgeeks.org/merge-sort
 
 var array = [1,4,2,3];
-selectionSort = function(arr)
-{
-  var i=0;
-  var size = arr.length;
-  var min = -Number.MAX_NUMBER;
-  var swap=-1;
+
+const _mergeArrays = (a, b) => {
+    const c = []
+    while (a.length && b.length) {
+      c.push(a[0] > b[0] ? b.shift() : a.shift())
+    }
+    //if we still have values, let's add them at the end of `c`
+    while (a.length) {
+      c.push(a.shift())
+    }
+    while (b.length) {
+      c.push(b.shift())
+    }
   
-  for(i=0;i<size;i++)
-  {   
-      min= arr[i];
-      var boo=false;
-      for(j=i+1;j<size;j++)
-      {
-          if(arr[j]<=min)
-          {
-              min =arr[j];
-              swap=j;
-              boo=true;
-          }
-      }
-      if(boo){
-      [arr[i],arr[swap]]= [arr[swap],arr[i]];
-      }
+    return c
   }
 
-  console.log(arr);
-}
-
-selectionSort(array);
+  const _mergeSort = (a) => {
+    if (a.length < 2) return a
+    const middle = Math.floor(a.length / 2)
+    const a_l = a.slice(0, middle)
+    const a_r = a.slice(middle, a.length)
+    const sorted_l = _mergeSort(a_l)
+    const sorted_r = _mergeSort(a_r)
+    return _mergeArrays(sorted_l, sorted_r)
+  }
+console.log(_mergeSort([1,4,2,3]));
